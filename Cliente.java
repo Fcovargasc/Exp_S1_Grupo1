0473fcbdc32a42eb38ce0ce1972744fcc3cf77f3
Fcovargasc <fr.vargasc@duocuc.cl>
2025-05-22 15:46:09 -0400

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,99 @@
+
+package exp_s1_grupo1;
+
+
+public class Cliente {
+    private String rut,nombre,apellidoPaterno,apellidoMaterno,domicilio,comuna;
+    private int telefono,cuenta,saldo;
+    
+  //constructor sin atributos  
+public Cliente(){} 
+
+//cosntructor con atributos
+public Cliente(String rut,String nombre , String apellidoPaterno,String apellidoMaterno,String domicilio,
+               String comuna,int telefono,int cuenta,int saldo){
+
+               this.rut=rut;
+               this.nombre=nombre;
+               this.apellidoPaterno=apellidoPaterno;
+               this.apellidoMaterno=apellidoMaterno;
+               this.domicilio=domicilio;
+               this.comuna=comuna;
+               this.telefono=telefono;
+               this.cuenta=cuenta;
+               this.saldo=saldo;
+                       }
+//creando setters and getters
+    public String getRut(){
+        return rut;
+}
+    public void setRut(String rut){
+        this.rut=rut;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre (String nombre){
+        this.nombre=nombre;
+        
+    }
+    public String getApellidoPaterno(){
+        return apellidoPaterno;
+    }
+    public void setApellidoPaterno(String apellidoPaterno){
+        this.apellidoPaterno=apellidoPaterno;
+    }
+    public String getApellidoMaterno(){
+        return apellidoMaterno;
+    }
+    public void setApellidoMaterno(String apellidoMaterno){
+        this.apellidoMaterno=apellidoMaterno;
+    }
+    public String getDomicilio(){
+        return domicilio;
+    }
+    public void setDomicilio (String domicilio){
+        this.nombre=nombre;
+        
+    }
+    public String getComuna(){
+        return comuna;
+    }
+    public void setComuna(String comuna){
+        this.comuna=comuna;
+    }
+    public int getTelefono(){
+        return telefono;
+    }
+    public void setTelefono(int telefono){
+        this.telefono=telefono;
+    }
+    public int getCuenta(){
+        return cuenta;
+    }
+    public void setCuenta(int cuenta){
+        this.cuenta=cuenta;
+    }
+    public int getSaldo(){
+        return saldo;
+    }
+    public void setSaldo(int saldo){
+        this.cuenta=cuenta;
+    }
+    
+    //creacion de metodos
+    
+    public void depositar(){
+        System.out.println("¡Deposito realizado de manera exitosa! ");
+        System.out.println("Ud tiene un saldo actual de ");
+    }
+    public void girar (){
+        System.out.println("¡Giro Realizado de manera exitosa!");
+        System.out.println("Usted tiene un saldo actual de ");
+    }
+    public void consultaSaldo(){
+        System.out.println("Saldo Actual");
+    }
+
+
+}
---
+++ Exp_S1_Grupo1.java
@@ -0,0 +1,107 @@
+
+package exp_s1_grupo1;
+
+import java.util.Scanner;
+
+
+public class Exp_S1_Grupo1 {
+
+   
+    public static void main(String[] args) {
+        //Bank Boston
+        //se debe poder hacer las sig operaciones 
+        //registrar cliente(registro de datos de una persona incluye cta corriente)
+        //ver datos de cliente ( permite visualizar los datos de un cliernte)
+        //depositar dinero en pesos chilenos  
+        //girar dinero ( si la cta esta en 0 y se trata de girar lanzar un mensaje y voplver al menu principal)
+        //consultar saldo 
+        //Ejemplo cta anita
+        // Rut: 19.767.453 - 0
+         //Nombre: Anita
+         //Apellido Paterno: Morales
+         //Apellido Materno: Fuentes
+         //Domicilio: Los Alerces #123
+         //Comuna: La Cisterna
+        // Teléfono: 94534242
+        // Cuenta Corriente: 123456789
+        Scanner scanner= new Scanner (System.in);
+        
+        int opcion;
+        String rut="",nombre="",apellidoPaterno="",apellidoMaterno="",domicilio="",comuna="";
+        int telefono=00000000,cuenta=000000000,saldo=0;
+        
+        
+        System.out.println("Bienvenidos A Bank Boston");
+        do{
+        System.out.println("Presione");
+        System.out.println("1 Para Registrar Cliente");
+        System.out.println("2 Para Ver datos del Cliente");
+        System.out.println("3 Para Depositar");
+        System.out.println("4 Para Girar");
+        System.out.println("5 Para Consultar saldo");
+        System.out.println("6 Para Salir");
+        opcion=scanner.nextInt();
+        Cliente cliente =new Cliente(rut,nombre,apellidoPaterno,apellidoMaterno,domicilio,comuna,telefono,cuenta,saldo);
+
+            switch(opcion){
+                case 1: 
+                    System.out.println("Registro de clientes");
+                    System.out.println("Ingrese su rut");
+                    scanner.nextLine();
+                    rut=scanner.nextLine();
+                    System.out.println("Ingrese su Nombre");
+                    nombre=scanner.nextLine();
+                    System.out.println("Ingrese Apellido Paterno");
+                    apellidoPaterno=scanner.nextLine();
+                    System.out.println("Ingrese Apellido Materno");
+                    apellidoMaterno=scanner.nextLine();
+                    System.out.println("Ingrese Su Domicilio");
+                    domicilio=scanner.nextLine();
+                    System.out.println("Ingrese Comuna");
+                    comuna=scanner.nextLine();
+                    System.out.println("Ingrese Numero de Telefono");
+                    telefono=scanner.nextInt();
+                    System.out.println("Ingrese Numero de Cuenta");
+                    cuenta=scanner.nextInt();
+                    
+                    break;
+                case 2:
+                    System.out.println("Datos del CLiente");
+                    
+                    System.out.println("Rut: "+cliente.getRut());
+                    System.out.println("Nombre: "+cliente.getNombre());
+                    System.out.println("Apellido Paterno: "+cliente.getApellidoPaterno());
+                    System.out.println("Apellido Materno: "+cliente.getApellidoMaterno());
+                    System.out.println("Domicilio: "+cliente.getDomicilio());
+                    System.out.println("Comuna: "+cliente.getComuna());
+                    System.out.println("Telefono: "+cliente.getTelefono());
+                    System.out.println("Numero de Cuenta Corriente: "+cliente.getCuenta());
+                    System.out.println("Saldo: "+cliente.getSaldo());
+                    break;
+                case 3:
+                    System.out.println("Deposito");
+                   cliente.depositar();
+                    break;
+                case 4:
+                    System.out.println("Giro");
+                    cliente.girar();
+                    break;
+                case 5: 
+                    System.out.println("Consulta saldo");
+                    cliente.consultaSaldo();
+                    break;
+                case 6 :
+                    System.out.println("Saliendo... Gracias por venir a Bank Boston");
+            }       
+        
+        }while(opcion!=6);
+    
+        
+   
+        
+        
+    }
+    
+    
+    
+}
